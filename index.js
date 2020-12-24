b0ea6a5043aa9f8fb1bbdc1ed1282c45c83f2628
Josh <billfermandez@gmail.com>
2020-12-23 20:07:26 -0600

    Add tutorial
    Key opening functionality still needs to be implemented but I don't have any keys to open to inspect the network calls

--- index.js
+++ index.js
@@ -1,44 +1,82 @@
+// Made by slendy. 2020, Hello to anyone who bothers to look here
+
 let regex = new RegExp("^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$");
 var token = '';
 var numKeys = 0;
+var username = '';
 window.addEventListener('load', function() {
     document.getElementById("submit").addEventListener('click', function(e){
-      e.preventDefault();
-      checkInput();
+        e.preventDefault();
+        checkInput();
     });
     document.getElementById("spin").addEventListener('click', function(e){
-          e.preventDefault();
-          openKey();
-        });
+        e.preventDefault();
+        openKey();
+    });
+    document.getElementById("login").addEventListener('click', function(e){
+        e.preventDefault();
+        tryLogin();
+    });
 });
 
-
-function checkInput(){
-  if(regex.test(document.getElementById("jwt").value)){
-    getKeys(document.getElementById("jwt").value);
-  } else {
-    alertTokenInvalid();
+function tryLogin(){
+  var user = document.getElementById("user").value;
+  var password = document.getElementById("password").value;
+  if(!user && !password){
+    alertLoginFailed();
+    return;
   }
-}
-
-function alertTokenInvalid(){
-  document.getElementById("error").style = "display: block";
-}
-
-function getKeys(jwt){
-  token = jwt;
   var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         if (this.readyState == 4) {
           if(this.status == 200) {
-            parseResponse(req.responseText);
+            var obj = JSON.parse(req.responseText);
+            console.log(obj);
+            token = obj.tokens.id;
+            console.log(token);
+            getKeys(token);
           } else {
-            alertTokenInvalid();
+            alertLoginFailed();
+          }
+        }
+    }
+    req.open("POST", "https://production.api.finalmouse.com/aimgods/auth/login/web", true);
+    req.send("{\"username\":\"" + user + "\",\"pwd\":\"" + password + "\"}");
+}
+
+function checkInput(){
+  if(regex.test(document.getElementById("jwt").value)){
+    token = document.getElementById("jwt").value;
+    getKeys();
+  } else {
+    alertBadToken();
+  }
+}
+
+function alertLoginFailed(){
+  document.getElementById("errorLogin").style = "display: block";
+}
+
+function alertBadToken(){
+  document.getElementById("errorToken").style = "display: block";
+}
+
+function getKeys(){
+  var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if(this.status == 200) {
+              var obj = JSON.parse(req.responseText);
+              username = obj.UserName;
+              numKeys = obj.GoldenKeys;
+              updatePage();
+          } else {
+            alertBadToken();
           }
         }
     }
     req.open("GET", "https://production.api.finalmouse.com/aimgods/auth/me", true);
-    req.setRequestHeader("authorization", jwt);
+    req.setRequestHeader("authorization", token);
     req.send();
 }
 
@@ -52,10 +90,7 @@ function displayResult(response){
     document.getElementById("spinResult").style = "color:red";
   }
   numKeys--;
-  document.getElementById("authKeys").innerHTML = "You have " + numKeys + " keys remaining";
-  if(numKeys == 0){
-    document.getElementById("spin").classList.add("disabled");
-  }
+  updatePage();
 }
 
 function openKey(){
@@ -74,10 +109,7 @@ function openKey(){
       req.send();
 }
 
-function parseResponse(response){
-  var obj = JSON.parse(response);
-  var username = obj.UserName;
-  numKeys = obj.GoldenKeys;
+function updatePage(){
   document.getElementById("bruh").style = "display: none;";
   document.getElementById("authHeader").innerHTML = "Welcome " + username;
   document.getElementById("authKeys").innerHTML = "You have " + numKeys + " keys remaining";
